5c6a061b35a1e4e3ab34789087d3ee30f7c6fd3d
irwanphan <irwanphan@gmail.com>
2023-10-08 21:17:41 +0700

    add remaining characters count on Title input

--- src/components/FormCreateNote/index.jsx
+++ src/components/FormCreateNote/index.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 const FormCreateNote = ({ onCreateNote }) => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [remainingCharacters, setRemainingCharacters] = useState(50);
+
+    const onTitleChange = (e) => {
+        const inputValue = e.target.value;
+        const remaining = 50 - inputValue.length;
+        if (remaining >= 0) {
+            setTitle(inputValue);
+            setRemainingCharacters(remaining);
+        }
+    };
+
     const onSubmiteHandler = (e) => {
         e.preventDefault();
         const data = {
             id: new Date().getTime(),
-            title: e.target.title.value,
-            body: e.target.body.value,
+            // title: e.target.title.value,
+            title: title,
+            // body: e.target.body.value,
+            body: body,
             archived: false,
             createdAt: new Date().toISOString()
         }
@@ -21,10 +36,22 @@ const FormCreateNote = ({ onCreateNote }) => {
         <div className="form-create-note">
             <form onSubmit={(e) => onSubmiteHandler(e)}>
                 <label htmlFor="title">Title</label>
-                <input id="title" type="text" placeholder="Title" />
+                <small>remaining characters: {remainingCharacters}</small>
+                <input 
+                    id="title" 
+                    type="text" 
+                    placeholder="Title" 
+                    value={title}
+                    onChange={(e) => onTitleChange(e)}
+                />
 
                 <label htmlFor="body">Body</label>
-                <textarea id="body" placeholder="Body" />
+                <textarea 
+                    id="body" 
+                    placeholder="Body" 
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                />
 
                 <button type="submit">Submit</button>
             </form>
