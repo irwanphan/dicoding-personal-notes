b9a599c7803fc0cd0fa59873468fa26b0b2e8926
irwanphan <irwanphan@gmail.com>
2023-10-08 21:04:07 +0700

    delete button styling

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { showFormattedDate } from '../../utils';
+import { FiTrash } from 'react-icons/fi';
 
 const Note = ({item, onDeleteNote }) => {
     // console.log(item)
@@ -11,7 +12,9 @@ const Note = ({item, onDeleteNote }) => {
             <div className="note-item__title">{item.title}</div>
             <div className="note-item__date">{showFormattedDate(item.createdAt)}</div>
             <div className="note-item__body">{item.body}</div>
-            <button onClick={onDeleteHandler}>Delete</button>
+            <div className='note-item__action'>
+                <button className='note-item__button' onClick={onDeleteHandler}><FiTrash/></button>
+            </div>
         </div>
     )
 }
