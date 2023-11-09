d3bbf866939011a6cba505f88b1a2e8c39b3837a
irwanphan <irwanphan@gmail.com>
2023-11-09 16:04:27 +0700

    locale in 404

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useLocale } from "../../contexts/LocaleContext";
 
 const NotFoundPage = () => {
+    const { isIndonesiaLocale } = useLocale();
+
     return (
         <div>
-            <Link to={`/`}>Go Home</Link>
+            <Link to={`/`}>{isIndonesiaLocale ? 'Kembali' : 'Go Home'}</Link>
             <div className="divider"></div>
             <h1 className="not-found">404</h1>
-            <h3 className="not-found">Page not found, you're lost</h3>
+            <h3 className="not-found">{isIndonesiaLocale ? "Anda tersesat, halaman tidak ditemukan" : "Page not found, you're lost"}</h3>
         </div>
     )
 }
