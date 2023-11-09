c04ae258d3bc915a9c9cd03b6c149193d8badde0
irwanphan <irwanphan@gmail.com>
2023-11-09 15:12:59 +0700

    styling color for dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,12 @@ import AppRoutes from './routes';
 import { getAccessToken, getUserLogged } from './utils/network-data';
 import { useLocation } from 'react-router-dom';
 import UserBadge from './components/UserBadge';
-import { useTheme } from './contexts/themeContext';
 
 const App = () => {
   const [ user, setUser ] = useState(null);
   const [ isChecking, setIsChecking ] = useState(true);
   const [ isLoading, setIsLoading ] = useState(true);
   const { pathname } = useLocation();
-  const { isDarkTheme, toggleTheme } = useTheme();
 
   const loginCheck = async() => {
     try {
@@ -49,11 +47,9 @@ const App = () => {
   }, [isChecking])
 
   if (isLoading) return (
-    <div className={isDarkTheme ? 'dark-theme' : 'light-theme'} >
-      <div className='container' >
-        <Header/>
-        <p>Authenticating ...</p>
-      </div>
+    <div className='container' >
+      <Header/>
+      <p>Authenticating ...</p>
     </div>
   )
 
--- src/components/ThemeToggler/index.jsx
+++ src/components/ThemeToggler/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useTheme } from "../../contexts/themeContext";
+import { FiMoon, FiSun } from "react-icons/fi";
 
 const ThemeToggler = () => {
     const { isDarkTheme, toggleTheme } = useTheme();
 
     return (
-        <div className={isDarkTheme ? 'dark-theme' : 'light-theme'}>
-            <button onClick={toggleTheme}>Toggle Theme</button>
-            <p>{isDarkTheme ? 'Dark Theme' : 'Light Theme'}</p>
+        <div className={isDarkTheme ? 'dark-theme theme-toggler-container' : 'light-theme theme-toggler-container'}>
+            <button className="theme-toggler" onClick={toggleTheme}>Change Theme</button>
+            <p>{isDarkTheme ? <FiMoon /> : <FiSun />}</p>
         </div>
     );
 }
